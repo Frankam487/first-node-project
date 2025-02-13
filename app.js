3c2564d1584c56ac9c0fee46a18cd3e3199299cc
Frankam487 <fkamgang06@gmail.com>
2025-02-13 16:06:03 +0100

    blog app

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ const optionBd = {
   port: 3306,
   database: 'node_bd'
 };
-//extraction des donnees du formulaire
+//ce bout de code permet de recuperer les donnees du formulaire(indispensables pour les formulaires)
 app.use(express.urlencoded({ extended: false }));
 //definition du middleware
 app.use(myconnection(mysql, optionBd, 'pool'))
 
 //definition du moteur d'affichage
 app.set('view engine', 'ejs');
-app.set('views', 'IHM')
+app.set('views', 'IHM');
 
 app.get('/', (req, res) => {
 
@@ -37,17 +37,54 @@ app.get('/', (req, res) => {
     }
   });
 });
-
-app.post('/node', (req, res) => {
-console.log(req.body);
+app.delete('/node/:id', (req, res) => { 
+  let id = req.params.id;
+  req.getConnection((err, connection) => {
+    if (err) {
+      console.log(err);
+    } else {
+      connection.query('DELETE FROM node WHERE id = ?', [id], (err, resultat) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.status(300).json({routeRacine: './'});
+        }
+      })
+    }
+  });
 })
+app.post('/node', (req, res) => {
+  let id = req.body.id === "" ? null : req.body.id;
+  let titre = req.body.titre;
+  let description = req.body.description;
+
+let reqSQL = id === null ? "INSERT INTO node(id, titre, description)VALUES(?, ?, ?)" : "UPDATE node SET titre = ?, description = ? WHERE id = ?";
+
+  
+  let donnees = id === null ? [null, titre, description] : [titre, description, id];
+  req.getConnection((err, connection) => {
+    if (err) {
+      console.log(err);
+    } else {
+      connection.query(
+        reqSQL,
+        donnees, (err, resultat) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.status(300).redirect('./');
+        }
+      })
+    }
+  });
+});
 
 app.get('/about', (req, res) => {
   const hour = new Date().toLocaleDateString("fr-FR", {
     hour: 'numeric',
     minute: '2-digit',
     second: 'numeric'
-  })
+  });
   res.status(200).render('about', {hour});
 });
 app.get('*', (req, res) => {
